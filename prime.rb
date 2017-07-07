c2153277738e1fd7c18b5828f6b2ffbd0d621551
Hui Wang <huiw3939@gmail.com>
2017-07-07 15:24:38 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  if num > 1
+    arr = (2..(num-1)).to_a
+    arr.each do |i|
+      return false if (num % i) == 0
+    end
+    return true
+  else
+    return false
+end
+end
